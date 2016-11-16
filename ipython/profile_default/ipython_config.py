d44d6351d92d624c3dde415b22475b01fd5ddf66
JWCook <jordan.cook@pioneer.com>
2016-11-16 15:31:38 -0600

    Some more git aliases & misc settings; autoreload modules in IPython

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,5 +1,10 @@
 # Configuration file for ipython.
 
+# Autoreload modules
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+
 #------------------------------------------------------------------------------
 # Configurable configuration
 #------------------------------------------------------------------------------
