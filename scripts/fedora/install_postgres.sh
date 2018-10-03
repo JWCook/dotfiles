ad8b4eaa2de7595223dac6951c5fb865fcdee8d3
cookjo <jordan.cook@pioneer.com>
2018-10-03 14:44:48 -0500

    Misc bashrc & other adjustments

--- scripts/fedora/install_postgres.sh
+++ scripts/fedora/install_postgres.sh
@@ -7,6 +7,7 @@
 # Install Postgres 10 for Fedora 28; for other distros, see https://yum.postgresql.org/repopackages.php
 PG_REPO='https://download.postgresql.org/pub/repos/yum/10/fedora/fedora-28-x86_64/pgdg-fedora10-10-4.noarch.rpm'
 PG_PKGS='postgresql10-devel postgresql10-server postgis24_10-devel freetds-devel unixODBC-devel'
+# PG_PKGS='postgresql-devel postgresql-server postgis-devel freetds-devel unixODBC-devel'
 
 sudo dnf install -y $PG_REPO
 sudo dnf update -y
