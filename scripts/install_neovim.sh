b52c800a5006c3d9c1b9f1053bbb9655cfd1ad85
Jordan Cook <jordan.cook@pioneer.com>
2020-05-28 12:20:28 -0500

    Install and update neovim python dependencies in virtualenv instead of user site-packages

--- scripts/install_neovim.sh
+++ scripts/install_neovim.sh
@@ -4,3 +4,6 @@
 source install_appimage.sh
 install-appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 python3 -m venv ~/.virtualenvs/nvim
+source ~/.virtualenvs/nvim/bin/activate
+pip install -U pip pynvim vim-vint
+deactivate
--- scripts/install_npm_packages.sh
+++ scripts/install_npm_packages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # TODO: better method of handling global npm packages?
 
-PKGS_JS='doctoc inherits instant-markdown-d'
+PKGS_JS='doctoc inherits instant-markdown-d neovim'
 sudo npm install -g $PKGS_JS
