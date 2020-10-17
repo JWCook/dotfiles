5ba33bb62efd8da8cc220ef77343f28db434a0e5
Jordan Cook <jordan.cook@pioneer.com>
2020-10-17 11:07:32 -0500

    Extend sudoers session time

---
+++ scripts/configure_fonts_wsl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cat << EOF > /etc/fonts/local.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+    <dir>/mnt/c/Windows/Fonts</dir>
+</fontconfig>
+EOF
