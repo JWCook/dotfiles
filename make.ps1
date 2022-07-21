79f83adfe15adcfc6573a3b1b86afb7c4d7b153f
Jordan Cook <jordan.cook.git@proton.me>
2022-07-21 17:15:24 -0500

    Add some common git aliases for Powershell

---
+++ make.ps1
@@ -0,0 +1,18 @@
+$dotfiles = "D:\Workspace\dotfiles"
+$profile_src = $dotfiles\powershell\powershell_aliases.ps1
+
+$win_docs = [Environment]::GetFolderPath("MyDocuments")
+$profile_dir = $win_docs\WindowsPowerShell
+$profile_dest = $profile_dir\Microsoft.PowerShell_profile.ps1
+
+
+If (!(test-path $profile_dir)) {
+    md $profile_dir
+}
+
+If (test-path $profile_dest) {
+    rm $profile_dest
+}
+
+mklink /J $profile_src $profile_dest
+
---
+++ powershell/powershell_aliases.ps1
@@ -0,0 +1,20 @@
+Function func_gs {git status}
+Set-alias gs func_gs
+
+Function func_gp {git pull}
+Set-alias gp func_gp -force -option 'Constant','AllScope'
+
+Function func_gsw {git switch}
+Set-alias gsw func_gsw
+
+Function func_gsw {git switch -c}
+Set-alias gsc func_gsc
+
+Function func_gmend {git commit --amend}
+Set-alias gmend func_gmend
+
+Function func_gunstage {gunstage git reset HEAD}
+Set-alias gunstage func_gunstage
+
+Function func_guncommit {gunstage git reset --soft HEAD~}
+Set-alias guncommit func_guncommit
