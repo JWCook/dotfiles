feed564fe83f1b6c99f5d93e8c8360714e3b69b1
Jordan Cook <jordan.cook.git@proton.me>
2023-10-21 14:07:17 -0500

    No longer compiling separate vim installation

--- scripts/install_vim_plug.sh
+++ scripts/install_vim_plug.sh
@@ -9,5 +9,3 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 nvim +PlugUpdate +PlugUpgrade +UpdateRemotePlugins +qall
 test -x /usr/bin/vim && \
     /usr/bin/vim +PlugUpdate +PlugUpgrade +UpdateRemotePlugins +qall
-test -x //usr/local/bin/vim8 && \
-    /usr/local/bin/vim8 +PlugUpdate +PlugUpgrade +UpdateRemotePlugins +qall
