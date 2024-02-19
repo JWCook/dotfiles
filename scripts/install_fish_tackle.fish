4a17c3c301731522d5b029c9ae986155802ccd11
Jordan Cook <jordan.cook.git@proton.me>
2024-02-18 22:56:13 -0600

    Don't symlink virtualfish config

--- scripts/install_fish_tackle.fish
+++ scripts/install_fish_tackle.fish
@@ -1,7 +1,6 @@
 #!/usr/bin/env fish
 set CONF_DIR ~/.config/fish
 set FISH_PROMPT $CONF_DIR/functions/fish_prompt.fish
-set VF_LOADER $CONF_DIR/conf.d/virtualfish-loader.fish
 
 # First remove any previous symlinks and create parent dirs
 rm -f $CONF_DIR/{config.fish,style.fish} $FISH_PROMPT $VF_LOADER 2> /dev/null
@@ -11,7 +10,6 @@ mkdir -p $CONF_DIR/{completions,conf.d,functions}
 ln -s (pwd)/fish/config.fish $CONF_DIR/config.fish
 ln -s (pwd)/fish/style.fish  $CONF_DIR/style.fish
 ln -s (pwd)/fish/functions/fish_prompt.fish $FISH_PROMPT
-ln -s (pwd)/fish/conf.d/virtualfish-loader.fish $VF_LOADER
 
 # Copy over completions
 cp fish/completions/* $CONF_DIR/completions/
