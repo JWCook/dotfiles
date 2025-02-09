112a1dc5c2b47b7fc63624e9703407d3bb4750c9
Jordan Cook <jordan.cook.git@proton.me>
2025-02-09 14:04:09 -0600

    Use npiperelay to forward Windows SSH agent to WSL

--- fish/config_wsl.fish
+++ fish/config_wsl.fish
@@ -12,3 +12,13 @@ alias cov-open-wsl='powershell.exe /c start test-reports/index.html'
 
 # Start Docker daemon automatically when logging in if not running.
 wsl.exe -u root -e sh -c "service docker status > /dev/null || service docker start"
+
+# Use npiperelay to forward Windows SSH agent
+set -Ux SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+command ss -a | grep -q $SSH_AUTH_SOCK
+if test $status -ne 0
+    rm -f $SSH_AUTH_SOCK
+    set npiperelaypath $(wslpath "C:/npiperelay")
+    setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork \
+        EXEC:"$npiperelaypath/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent" nofork &
+end
