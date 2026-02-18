f41ccda4ab5842db5d177a045fbaab9fdd019d77
Jordan Cook <jordan.cook@corteva.com>
2026-02-17 18:32:45 -0600

    Remove conflicting tmux bindings

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -174,16 +174,4 @@ return {
     },
   },
 
-  -- Tmux navigation
-  {
-    "christoomey/vim-tmux-navigator",
-    keys = {
-      { "<c-h>",  "<cmd>TmuxNavigateLeft<cr>",     desc = "Tmux navigate left" },
-      { "<c-j>",  "<cmd>TmuxNavigateDown<cr>",     desc = "Tmux navigate down" },
-      { "<c-k>",  "<cmd>TmuxNavigateUp<cr>",       desc = "Tmux navigate up" },
-      { "<c-l>",  "<cmd>TmuxNavigateRight<cr>",    desc = "Tmux navigate right" },
-      { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Tmux navigate previous" },
-    },
-  },
-
 }
