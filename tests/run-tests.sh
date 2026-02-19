507107bd445e75cb97c19a8562ea2cd69aaf171e
Jordan Cook <jordan.cook.git@proton.me>
2026-02-18 23:55:51 -0600

    Add test container for EndeavourOS

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,6 +2,8 @@
 # Run containerized setup script tests
 
 cd ~/dotfiles
+docker build -t test-setup-endeavouros -f tests/Dockerfile-endeavouros .
+docker run --rm test-setup-endeavouros
 docker build -t test-setup-debian -f tests/Dockerfile-debian .
 docker run --rm test-setup-debian
 docker build -t test-setup-ubuntu -f tests/Dockerfile-ubuntu .
