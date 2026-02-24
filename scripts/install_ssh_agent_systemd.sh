569003f003757cb3b3bf56c0c0a9239b96222fb0
Jordan Cook <jordan.cook.git@proton.me>
2026-02-23 23:29:27 -0600

    Add socket location to systemd config

--- scripts/install_ssh_agent_systemd.sh
+++ scripts/install_ssh_agent_systemd.sh
@@ -6,5 +6,8 @@ cp scripts/ssh-agent.service $USER_SYSTEMD_DIR/
 systemctl --user enable ssh-agent
 systemctl --user start ssh-agent
 
-# Add to bashrc or other startup script:
+mkdir -p ~/.config/environment.d
+echo 'SSH_AUTH_SOCK=/run/user/1000/ssh-agent.socket' > ~/.config/environment.d/ssh-agent.conf
+
+# And/or add to bashrc or other startup script:
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
